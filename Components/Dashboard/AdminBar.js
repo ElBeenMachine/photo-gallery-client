2bbbc106af4bd833903302919884af91d98e91ae
Ollie Beenham <beenhamow@outlook.com>
2023-05-08 12:32:53 +0100

    Made some styling changes and correctly report a 401 error

--- Components/Dashboard/AdminBar.js
+++ Components/Dashboard/AdminBar.js
@@ -1,8 +1,8 @@
-const { Stack } = require("@chakra-ui/react");
+const { Stack, useColorModeValue } = require("@chakra-ui/react");
 
 const AdminBar = ({ children }) => {
     return (
-        <Stack direction={"row"} w={"100%"} justifyContent={"end"} p={5} pb={0}>
+        <Stack direction={"row"} w={"100%"} justifyContent={"end"} p={5} borderBottomWidth={1} borderStyle={"solid"} borderColor={useColorModeValue("gray.200", "gray.700")} bg={useColorModeValue("gray.50", "gray.900")}>
             {children}
         </Stack>
     )
--- utils/fetchHandler.js
+++ utils/fetchHandler.js
@@ -1,5 +1,12 @@
+import { signOut } from "next-auth/react";
+
 const handleError = (response) => {
     if (!response.ok) { 
+        if(response.status == 401) {
+            signOut();
+            throw Error("Unauthorized, please log in again.");
+        }
+
         throw Error(response.statusText);
     } else {
         return response.json();
